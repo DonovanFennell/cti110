2f078a999c3847cd150b7db57cde170899d94468
DonovanFennell <99267919+DonovanFennell@users.noreply.github.com>
2022-02-11 11:41:48 -0500

    Add files via upload

---
+++ P1HW2_FennellDonovan.py
@@ -0,0 +1,15 @@
+# A brief description of the project
+    # 02/10/2022
+    # CTI-110 P1HW2 - Pizza Order
+    # Donovan Fennell
+    #
+Students = int(input('How many students do you want to order pizza for? '))
+Slices = 3
+Pizza = Students * Slices / 6
+
+
+print('------Pizza Order---------')
+print('Number of Students : ',(Students))
+print('Pizza Slices Needed: ',(Students * Slices))
+print('Pizzas Needed : ',(Pizza)) 
+print('-------------------------')
