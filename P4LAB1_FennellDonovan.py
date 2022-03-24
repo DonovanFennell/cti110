511ec21cb3620e4cff540b55149b52aec77de878
DonovanFennell <99267919+DonovanFennell@users.noreply.github.com>
2022-03-24 08:40:56 -0400

    Add files via upload

---
+++ P4LAB1_FennellDonovan.py
@@ -0,0 +1,7 @@
+user_text = input()
+
+char_count = 0
+for character in user_text:
+    if (character != ' ') and (character != '.') and (character != ',') and (character != '!'):
+        char_count += 1
+print(char_count)
